205881eba9cc2e76282c06518171710c4efd5718
pedropk <pedropk@gmail.com>
2018-09-01 11:59:41 -0300

    Created an Example Entity to test the configuration

--- br.unibratec.psc/src/br/psc/model/entity/ExampleEntity.java
+++ br.unibratec.psc/src/br/psc/model/entity/ExampleEntity.java
@@ -16,29 +16,73 @@ public class ExampleEntity implements EntityInterface {
 	public Object getPrimaryKey() {
 		return getId();
 	}
-
+	
 	public String getId() {
 		return this.id;
 	}
-
+	
 	public void setId(String pId) {
 		this.id = pId;
 	}
-
+	
 	public String getName() {
 		return this.name;
 	}
-
+	
 	public void setName(String pName) {
 		this.name = pName;
 	}
-
+	
 	public Date getRegistrationDate() {
 		return this.registrationDate;
 	}
-
+	
 	public void setRegistrationDate(Date pRegistrationData) {
 		this.registrationDate = pRegistrationData;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((registrationDate == null) ? 0 : registrationDate.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		
+		if (obj == null)
+			return false;
+		
+		if (getClass() != obj.getClass())
+			return false;
+		
+		ExampleEntity other = (ExampleEntity) obj;
+		
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		
+		if (registrationDate == null) {
+			if (other.registrationDate != null)
+				return false;
+		} else if (!registrationDate.equals(other.registrationDate))
+			return false;
+		
+		return true;
+	}
+	
 }
