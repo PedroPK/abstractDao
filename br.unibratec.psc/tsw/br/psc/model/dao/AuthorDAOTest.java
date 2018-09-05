da66a63352a91e24848996f62586d2835f6b9472
pedropk <pedropk@gmail.com>
2018-09-05 19:11:39 -0300

    Implemented new Tests, for RollbackException of Transient @Many attributes, and also PersistenceException for Entities with @Generated @Id manually set.

--- br.unibratec.psc/tsw/br/psc/model/dao/AuthorDAOTest.java
+++ br.unibratec.psc/tsw/br/psc/model/dao/AuthorDAOTest.java
@@ -1,6 +1,12 @@
 package br.psc.model.dao;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
+
+import javax.persistence.PersistenceException;
+import javax.persistence.RollbackException;
+
+import org.hibernate.PersistentObjectException;
+import org.hibernate.TransientObjectException;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -19,19 +25,57 @@ public class AuthorDAOTest {
 		
 		Author selectedAuthor = (Author) dao.selectByEntity(author.getClass(), author);
 		
-		Assert.assertNotNull(selectedAuthor);
+		assertNotNull(selectedAuthor);
 		
 		/* Cannot be used, once the Attribute of Books Collection is not null, but will invoke an InvocationException
 		Assert.assertEquals(author, selectedAuthor);*/
 		
-		Assert.assertNotSame(author, selectedAuthor);
+		assertNotSame(author, selectedAuthor);
+		
+		dao.deleteObject(author);
 	}
 	
-	@Ignore
-	@Test
+	@Test(expected=RollbackException.class)
 	public void testTransientObjectException() {
-		Author author = new Author();
-		Book book = new Book();
+		Author author = new Author("Alan Moore");
+		Book book = new Book("Promethea");
+		author.addBook(book);
+		
+		AuthorDAO dao = new AuthorDAO();
+		dao.insert(author);
+		
+		dao.deleteObject(author);
+	}
+	
+	@Test(expected=PersistenceException.class)
+	public void testDetachedEntityPersist() {
+		Author author = new Author(1, "Neil Gaiman");
+		AuthorDAO dao = new AuthorDAO();
+		
+		dao.insert(author);
+		
+		dao.deleteObject(author);
+	}
+	
+	@Test
+	public void testUpdateUnexistentAuthor() {
+		Author author = new Author(1, "Brian Greene");
+		AuthorDAO dao = new AuthorDAO();
+		
+		dao.update(author);
+		
+		Author selectedAuthor = (Author) dao.selectByEntity(author.getClass(), author);
+		
+		assertNotNull(selectedAuthor);
+		
+		/* Cannot be used, once the Attribute of Books Collection is not null, but will invoke an InvocationException
+		Assert.assertEquals(author, selectedAuthor);*/
+		
+		assertNotSame(author, selectedAuthor);
+		assertEquals(author.getId(), selectedAuthor.getId());
+		assertEquals(author.getName(), selectedAuthor.getName());
+		
+		dao.deleteObject(author);
 	}
 	
 }
